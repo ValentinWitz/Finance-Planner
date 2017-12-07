1758ae1345e8b5128c6f042d408e5add498aea4a
ValentinWitz <s1610237037@students.fh-hagenberg.at>
2017-12-07 11:29:53 +0100

    +pin login +added all view controllers (classes are still missing except of the Manager View)
    -VW

---
+++ Finance Planner/ManagePlanner.swift
@@ -0,0 +1,47 @@
+//
+//  ManagePlanner.swift
+//  Finance Planner
+//
+//  Created by Valentin Witzeneder on 07.12.17.
+//  Copyright © 2017 Valentin Witzeneder. All rights reserved.
+//
+
+import UIKit
+
+class ManagePlanner: UIViewController {
+    
+    @IBOutlet weak var incomesButton: UIButton!
+    @IBOutlet weak var outcomesButton: UIButton!
+    @IBOutlet weak var statisticButton: UIButton!
+    
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        roundButtons()
+    }
+
+    override func didReceiveMemoryWarning() {
+        super.didReceiveMemoryWarning()
+        // Dispose of any resources that can be recreated.
+    }
+    
+
+    func roundButtons() {
+        incomesButton.layer.cornerRadius = 20
+        outcomesButton.layer.cornerRadius = 20
+        statisticButton.layer.cornerRadius = 20
+    }
+    
+    
+    
+    /*
+    // MARK: - Navigation
+
+    // In a storyboard-based application, you will often want to do a little preparation before navigation
+    override func prepare(for segue: UIStoryboardSegue, sender: Any?) {
+        // Get the new view controller using segue.destinationViewController.
+        // Pass the selected object to the new view controller.
+    }
+    */
+
+}
--- Finance Planner/ViewController.swift
+++ Finance Planner/ViewController.swift
@@ -9,10 +9,127 @@
 import UIKit
 
 class ViewController: UIViewController {
-
+    
+    
+    @IBOutlet weak var pinLabel: UILabel!
+    
+    @IBOutlet weak var Button1: UIButton!
+    @IBOutlet weak var Button2: UIButton!
+    @IBOutlet weak var Button3: UIButton!
+    @IBOutlet weak var Button4: UIButton!
+    @IBOutlet weak var Button5: UIButton!
+    @IBOutlet weak var Button6: UIButton!
+    @IBOutlet weak var Button7: UIButton!
+    @IBOutlet weak var Button8: UIButton!
+    @IBOutlet weak var Button9: UIButton!
+    @IBOutlet weak var submitButton: UIButton!
+    
+    
+    @IBAction func Button1Touch(_ sender: Any) {
+        pinLabel.text! += "1"
+    }
+    
+    @IBAction func Button2Touch(_ sender: Any) {
+        pinLabel.text! += "2"
+    }
+    
+    @IBAction func Button3Touch(_ sender: Any) {
+        pinLabel.text! += "3"
+    }
+    
+    @IBAction func Button4Touch(_ sender: Any) {
+        pinLabel.text! += "4"
+    }
+    
+    @IBAction func Button5Touch(_ sender: Any) {
+        pinLabel.text! += "5"
+    }
+    
+    @IBAction func Button6Touch(_ sender: Any) {
+        pinLabel.text! += "6"
+    }
+    
+    @IBAction func Button7Touch(_ sender: Any) {
+        pinLabel.text! += "7"
+    }
+    
+    @IBAction func Button8Touch(_ sender: Any) {
+        pinLabel.text! += "8"
+    }
+    
+    @IBAction func Button9Touch(_ sender: Any) {
+        pinLabel.text! += "9"
+    }
+    
+    
+    @IBAction func submitButton(_ sender: Any) {
+        let pinCode = pinLabel.text
+        if(pinCode == nil || pinCode!.count < 4) {
+            let alert = UIAlertController(title: "ERROR", message: "Password has to be at least 4 numbers", preferredStyle: .alert)
+            alert.addAction(UIAlertAction(title: "OK", style: UIAlertActionStyle.default, handler: { (action) in
+                alert.dismiss(animated: true, completion: nil)
+            }))
+            self.present(alert, animated: true, completion: nil)
+            pinLabel.text = ""
+        } else {
+            let pinCodeInt:Int? = Int(pinCode!)
+            
+            let defaults = UserDefaults.standard
+            if let password = defaults.object(forKey: "password") as? Int {
+                if(password == pinCodeInt){
+                    self.performSegue(withIdentifier: "showPlanner", sender: self)
+                }
+                else{
+                    let alert = UIAlertController(title: "ERROR", message: "Invalid password", preferredStyle: UIAlertControllerStyle.alert)
+                    alert.addAction(UIAlertAction(title: "OK", style: UIAlertActionStyle.default, handler: { (action) in
+                        alert.dismiss(animated: true, completion: nil)
+                    }))
+                    self.present(alert, animated: true, completion: nil)
+                    pinLabel.text = ""
+                }
+            } else {
+                if(!(pinCode == nil || pinCode!.count < 4)) {
+                defaults.set(pinCodeInt, forKey: "password")
+                defaults.synchronize()
+                self.performSegue(withIdentifier: "showPlanner", sender: self)
+                }
+            }
+        }
+    }
+    
     override func viewDidLoad() {
         super.viewDidLoad()
-        // Do any additional setup after loading the view, typically from a nib.
+        roundButtons()
+        NotificationCenter.default.addObserver(forName: NSNotification.Name(rawValue: "resetPinBackground"), object: nil, queue: nil) { (notification) in
+            self.pinLabel.text = ""
+        }
+    }
+    
+    
+    //At first start tell the user to make a password
+    override func viewDidAppear(_ animated: Bool) {
+        if(UserDefaults.standard.object(forKey: "password") == nil){
+            print("Test")
+            let alert = UIAlertController(title: "Safety", message: "Welcome to Finance Planner enter a at least 4 digit password for your Finance Manager", preferredStyle: .alert)
+            alert.addAction(UIAlertAction(title: "OK", style: UIAlertActionStyle.default, handler: { (action) in
+                alert.dismiss(animated: true, completion: nil)
+            }))
+            self.present(alert, animated: true, completion: nil)
+        }
+    }
+    
+    //roundButtons modifies the Buttons so they appear smoother
+    func roundButtons(){
+        Button1.layer.cornerRadius = 10
+        Button2.layer.cornerRadius = 10
+        Button3.layer.cornerRadius = 10
+        Button4.layer.cornerRadius = 10
+        Button5.layer.cornerRadius = 10
+        Button6.layer.cornerRadius = 10
+        Button7.layer.cornerRadius = 10
+        Button8.layer.cornerRadius = 10
+        Button9.layer.cornerRadius = 10
+        submitButton.layer.cornerRadius = 10
     }
 
     override func didReceiveMemoryWarning() {
